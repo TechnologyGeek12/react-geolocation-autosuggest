753ca098e4aec554a5886449cb83824a1f8f4c6a
TechnologyGeek12 <gauravtanwar07@gmail.com>
2019-03-28 13:48:43 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import GeoLocation from "./lib/index";
+
+import "./index.css";
+
+ReactDOM.render(<GeoLocation />, document.getElementById("root"));
---
+++ src/lib/components/GeoLocation.js
@@ -0,0 +1,417 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import deburr from 'lodash/deburr';
+import Autosuggest from 'react-autosuggest';
+import match from 'autosuggest-highlight/match';
+import parse from 'autosuggest-highlight/parse';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import MenuItem from '@material-ui/core/MenuItem';
+import { withStyles } from '@material-ui/core/styles';
+
+let selectedItemValue = {};
+let isDisable = false;
+const styles = theme => ({
+    root: {
+        // height: 250,
+        flexGrow: 1,
+        // width:300
+    },
+    container: {
+        position: 'relative',
+    },
+    suggestionsContainerOpen: {
+        position: 'absolute',
+        zIndex: 1,
+        marginTop: theme.spacing.unit,
+        left: 0,
+        right: 0,
+    },
+    suggestion: {
+        display: 'block',
+    },
+    suggestionsList: {
+        margin: 0,
+        padding: 0,
+        listStyleType: 'none',
+    },
+    divider: {
+        height: theme.spacing.unit * 2,
+    },
+    displayInlineBlock: {
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'space-between',
+        marginTop: 10
+    },
+    displaySeprateBlock: {
+        width: '100%'
+    },
+    input: { 
+        borderBottom: '1px solid #2196f3' 
+    },
+    focused: {
+        borderBottom: '1px solid #2196f3',
+        color:'#2196f3'
+    },
+    floatingLabelFocusStyle:{ focused:{
+        color: "#00ff00"
+    }}
+});
+
+function renderInputComponent(inputProps) {
+    const { classes, inputRef = () => { }, ref, ...other } = inputProps;
+
+    return (
+        <TextField
+            fullWidth
+            underlinefocusstyle={styles.underlineStyle}
+            label={isDisable ? 'Add Google Api key in your index.html file to activate this field' : 'Search...'}
+            InputProps={{
+                inputRef: node => {
+                    ref(node);
+                    inputRef(node);
+                },
+                classes: {
+                    // input: classes.input,
+                    // focused: classes.focused,
+                },
+            }}
+            {...other}
+        />
+    );
+}
+
+function renderSuggestion(suggestion, { query, isHighlighted }) {
+    const matches = match(suggestion.description, query);
+    const parts = parse(suggestion.description, matches);
+
+    return (
+        <MenuItem selected={isHighlighted} component="div">
+            <div>
+                {parts.map((part, index) =>
+                    part.highlight ? (
+                        <span key={String(index)} style={{ fontWeight: 500 }}>
+                            {part.text}
+                        </span>
+                    ) : (
+                            <strong key={String(index)} style={{ fontWeight: 300 }}>
+                                {part.text}
+                            </strong>
+                        ),
+                )}
+            </div>
+        </MenuItem>
+    );
+}
+
+function getSuggestionValue(suggestion) {
+    selectedItemValue = suggestion;
+    return suggestion.description;
+}
+
+class GeoLocation extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: [],
+            searchText: this.props.value || '',
+            single: '',
+            popper: '',
+            suggestions: [],
+            componentForm: {},
+        };
+
+        const google = window.google;
+        if (google) {
+            this.geocoder = new google.maps.Geocoder();
+
+            // Documentation for AutocompleteService
+            // https://developers.google.com/maps/documentation/javascript/places-autocomplete#place_autocomplete_service
+            this.service = new google.maps.places.AutocompleteService(null);
+        }
+        else {
+            isDisable = true;
+        }
+        // binding for functions
+        this.updateInput = this.updateInput.bind(this);
+        this.populateData = this.populateData.bind(this);
+        this.getCurrentDataState = this.getCurrentDataState.bind(this);
+        this.getLatLgn = this.getLatLgn.bind(this);
+        // this.onClose = this.onClose.bind(this);
+    }
+
+    getSuggestions = (value) => {
+        const inputValue = deburr(value.trim()).toLowerCase();
+        const inputLength = inputValue.length;
+        let count = 0;
+        return inputLength === 0
+            ? []
+            : this.state.suggestions.filter(suggestion => {
+                const keep =
+                    count < 5 && suggestion.description.slice(0, inputLength).toLowerCase() === inputValue;
+
+                if (keep) {
+                    count += 1;
+                }
+
+                return keep;
+            });
+    }
+
+    getCurrentDataState() {
+        return this.state.data;
+    }
+
+    getLatLgn(locationID, cb) {
+        this.geocoder.geocode({ placeId: locationID }, (results, status) => {
+            cb(results, status);
+        });
+    }
+
+    updateInput(searchText) {
+        if (searchText && searchText.trim().length > 0) {
+            this.setState({
+                searchText,
+            },
+                () => {
+                    const outerScope = this;
+                    this.service.getPlacePredictions({
+                        input: this.state.searchText,
+                        // componentRestrictions: { country: 'us' },
+                        // componentRestrictions:{ country: ["us","au"] },
+                        types: this.props.types,
+                    },
+                        (predictions, fd) => {
+
+                            if (predictions) {
+                                outerScope.populateData(predictions);
+                            }
+                        }
+                    );
+                });
+        } else {
+            this.setState({
+                searchText: ''
+            })
+        }
+    }
+
+    populateData(array) {
+        this.setState({ data: array });
+    }
+
+    handleSuggestionsFetchRequested = ({ value }) => {
+        this.setState({
+            suggestions: this.getSuggestions(value),
+        });
+    };
+
+    handleSuggestionsClearRequested = () => {
+        this.setState({
+            suggestions: [],
+        });
+    };
+
+    handleChange = name => (event, { newValue }) => {
+        let componentForm = {
+            street_number: 'long_name',
+            route: 'long_name',
+            locality: 'long_name',
+            administrative_area_level_1: 'long_name',
+            country: 'long_name',
+            postal_code: 'long_name',
+            lat: '0.0',
+            lng: '0.0',
+            description: ''
+        };
+        this.setState({
+            [name]: newValue,
+            componentForm: componentForm
+        });
+        this.updateInput(newValue);
+        this.props.onChange && this.props.onChange(newValue);
+    };
+
+    onItemSelection = (e) => {
+        let item = selectedItemValue;
+        let componentForm = {
+            street_number: 'long_name',
+            route: 'long_name',
+            locality: 'long_name',
+            administrative_area_level_1: 'long_name',
+            country: 'long_name',
+            postal_code: 'long_name',
+            lat: '0.0',
+            lng: '0.0',
+            description: ''
+        };
+        this.getLatLgn(item.place_id, (place) => {
+            for (let i = 0; i < place[0].address_components.length; i++) {
+                let addressType = place[0].address_components[i].types[0];
+                if (componentForm[addressType]) {
+                    let val = place[0].address_components[i][componentForm[addressType]];
+                    componentForm[addressType] = val;
+                }
+            }
+            if (!componentForm['postal_code'] || componentForm['postal_code'] === 'long_name') { componentForm['postal_code'] = '00000' }
+            // get lat and get lng
+            componentForm['lat'] = place[0].geometry.location.lat();
+            componentForm['lng'] = place[0].geometry.location.lng();
+            componentForm.description = item.description;
+            // this.props.getFullAddress(componentForm);
+        });
+        this.onSelect(componentForm);
+        setTimeout(() => {
+            this.setState({ componentForm: componentForm });
+        }, 300)
+    }
+
+    setFieldValue = (name, e) => {
+        let componentForm = this.state.componentForm;
+        componentForm[name] = e.target.value;
+        this.setState({ componentForm: componentForm });
+    }
+
+    onSelect = (data) => {
+        this.props.onSelect && this.props.onSelect(data);
+    }
+
+    render() {
+        const { classes } = this.props;
+        const autosuggestProps = {
+            renderInputComponent,
+            suggestions: this.state.data,
+            onSuggestionsFetchRequested: this.handleSuggestionsFetchRequested,
+            onSuggestionsClearRequested: this.handleSuggestionsClearRequested,
+            onSuggestionSelected: this.onItemSelection,
+            focusInputOnSuggestionClick: false,
+            getSuggestionValue,
+            renderSuggestion,
+        };
+        let count = 0;
+        // if (!this.props.fixedWidthFields) {
+        if (this.props.isCountryVisible)
+            count++;
+        if (this.props.isStateVisible)
+            count++;
+        if (this.props.isCityVisible)
+            count++;
+        if (this.props.isPinCodeVisible)
+            count++;
+        if (this.props.showAllFields)
+            count = 4;
+        // }
+        return (
+            <div>
+                <div className={classes.root}>
+                    <Autosuggest
+                        {...autosuggestProps}
+                        inputProps={{
+                            classes,
+                            // placeholder: 'Search...',
+                            value: this.state.single,
+                            onChange: this.handleChange('single'),
+                            disabled: isDisable
+                        }}
+                        theme={{
+                            container: classes.container,
+                            suggestionsContainerOpen: classes.suggestionsContainerOpen,
+                            suggestionsList: classes.suggestionsList,
+                            suggestion: classes.suggestion,
+                        }}
+                        renderSuggestionsContainer={options => (
+                            <Paper {...options.containerProps} square>
+                                {options.children}
+                            </Paper>
+                        )}
+                    />
+                </div>
+                {this.props.displayInline && <div className={classes.displayInlineBlock}>
+                    {(this.props.isCountryVisible || this.props.showAllFields) && <TextField
+                        label="Country"
+                        value={this.state.componentForm.country && this.state.componentForm.country !== 'long_name' ? this.state.componentForm.country : ''}
+                        onChange={(e) => this.setFieldValue('country', e)}
+                        style={{ width: `${count === 1 ? 100 : 100 / count - 1}%` }}
+                        disabled={(this.props.isCountryDisabled || this.props.isAllDisabled || !this.state.searchText) ? true : false}
+                    />}
+
+                    {(this.props.isStateVisible || this.props.showAllFields) && <TextField
+                        label="State"
+                        value={this.state.componentForm.administrative_area_level_1 && this.state.componentForm.administrative_area_level_1 !== 'long_name' ? this.state.componentForm.administrative_area_level_1 : ''}
+                        onChange={(e) => this.setFieldValue('administrative_area_level_1', e)}
+                        style={{ width: `${count === 1 ? 100 : 100 / count - 1}%` }}
+                        disabled={(this.props.isStateDisabled || this.props.isAllDisabled || !this.state.searchText) ? true : false}
+                    />}
+                    {(this.props.isCityVisible || this.props.showAllFields) && <TextField
+                        label="City"
+                        value={this.state.componentForm.locality && this.state.componentForm.locality !== 'long_name' ? this.state.componentForm.locality : ''}
+                        onChange={(e) => this.setFieldValue('locality', e)}
+                        style={{ width: `${count === 1 ? 100 : 100 / count - 1}%` }}
+                        disabled={(this.props.isCityDisabled || this.props.isAllDisabled || !this.state.searchText) ? true : false}
+                    />}
+                    {(this.props.isPinCodeVisible || this.props.showAllFields) && <TextField
+                        label="Pin code"
+                        value={this.state.componentForm.postal_code && this.state.componentForm.postal_code !== '00000' && this.state.componentForm.postal_code !== 'long_name' ? this.state.componentForm.postal_code : ''}
+                        onChange={(e) => this.setFieldValue('postal_code', e)}
+                        style={{ width: `${count === 1 ? 100 : 100 / count - 1}%` }}
+                        disabled={(this.props.isPinCodeDisabled || this.props.isAllDisabled || !this.state.searchText) ? true : false}
+                    />}
+                </div>}
+                {!this.props.displayInline && <div className={classes.displaySeprateBlock}>
+                    {(this.props.isCountryVisible || this.props.showAllFields) && <TextField
+                        label="Country"
+                        value={this.state.componentForm.country && this.state.componentForm.country !== 'long_name' ? this.state.componentForm.country : ''}
+                        onChange={(e) => this.setFieldValue('country', e)}
+                        style={{ width: '100%', marginTop: 10 }}
+                        disabled={(this.props.isCountryDisabled || this.props.isAllDisabled || !this.state.searchText) ? true : false}
+                    />}
+
+                    {(this.props.isStateVisible || this.props.showAllFields) && <TextField
+                        label="State"
+                        value={this.state.componentForm.administrative_area_level_1 && this.state.componentForm.administrative_area_level_1 !== 'long_name' ? this.state.componentForm.administrative_area_level_1 : ''}
+                        onChange={(e) => this.setFieldValue('administrative_area_level_1', e)}
+                        style={{ width: '100%', marginTop: 10 }}
+                        disabled={(this.props.isStateDisabled || this.props.isAllDisabled || !this.state.searchText) ? true : false}
+                    />}
+                    {(this.props.isCityVisible || this.props.showAllFields) && <TextField
+                        label="City"
+                        value={this.state.componentForm.locality && this.state.componentForm.locality !== 'long_name' ? this.state.componentForm.locality : ''}
+                        onChange={(e) => this.setFieldValue('locality', e)}
+                        style={{ width: '100%', marginTop: 10 }}
+                        disabled={(this.props.isCityDisabled || this.props.isAllDisabled || !this.state.searchText) ? true : false}
+                    />}
+                    {(this.props.isPinCodeVisible || this.props.showAllFields) && <TextField
+                        label="Pin code"
+                        value={this.state.componentForm.postal_code && this.state.componentForm.postal_code !== '00000' && this.state.componentForm.postal_code !== 'long_name' ? this.state.componentForm.postal_code : ''}
+                        onChange={(e) => this.setFieldValue('postal_code', e)}
+                        style={{ width: '100%', marginTop: 10 }}
+                        disabled={(this.props.isPinCodeDisabled || this.props.isAllDisabled || !this.state.searchText) ? true : false}
+                    />}
+                </div>}
+            </div>
+        );
+    }
+}
+
+GeoLocation.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+GeoLocation.defaultProps = {
+    showAllFields: true,
+    isAllDisabled: false,
+    isCountryVisible: false,
+    isStateVisible: false,
+    isCityVisible: false,
+    isPinCodeVisible: false,
+    isCountryDisabled: false,
+    isStateDisabled: false,
+    isCityDisabled: false,
+    isPinCodeDisabled: false,
+    displayInline: true,
+    // fixedWidthFields: true
+}
+
+export default withStyles(styles)(GeoLocation);
